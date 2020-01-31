fe785dd0077d9c77ce9c925bb21cda9638dba058
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-01-30 22:46:05 -0500

    - routing to api
    -method verb included

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php
 
-define("_ROOTFOLDER",$_SERVER['DOCUMENT_ROOT']);
-define("_URLBASE",$_SERVER['SERVER_NAME']);
+define("_ROOTFOLDER", $_SERVER['DOCUMENT_ROOT']);
+define("_URLBASE", $_SERVER['SERVER_NAME']);
 
 $request = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
 switch ($request) {
-    case '/' :
+    case '/':
         http_response_code(200);
         require __DIR__ . '/views/index.php';
         break;
-    case '' :
+    case '':
         http_response_code(200);
         require __DIR__ . '/views/index.php';
         break;
-    case '/carlos_ferraz_output' :
+    case '/carlos_ferraz_output':
         http_response_code(200);
         require __DIR__ . '/views/carlos_ferraz_output.php';
         break;
-    case 'http://'.$_SERVER['SERVER_NAME'].'/carlos_ferraz_output':
+    case 'http://' . $_SERVER['SERVER_NAME'] . '/carlos_ferraz_output':
         http_response_code(200);
         require __DIR__ . '/views/carlos_ferraz_output.php';
+    case '/api/students':
+        http_response_code(200);
+        require __DIR__ . '/api/students.php';
     default:
         http_response_code(404);
         require __DIR__ . '/views/404.php';
         break;
 }
-
-?>
